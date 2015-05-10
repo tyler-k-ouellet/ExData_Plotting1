02853e7a5cea1424bebcd4d9df17301d922d2baa
Tyler Ouellet <tyler.ouellet@eclinicalworks.com>
2015-05-10 19:33:21 -0400

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -3,15 +3,16 @@ library(datasets)
 hpc <- read.csv("~/Downloads/household_power_consumption.txt", sep=";", na.strings="?")
 hpc$Date <- as.Date(hpc$Date, "%d/%m/%Y")
 hpc <- hpc[hpc$Date <= as.Date("2007-02-02") & hpc$Date >= as.Date("2007-02-01"), ]
-hpc$Time <- strptime(hpc$Time, format = "%H:%M:%S", tz = "GMT")
-hpc$Time <- format(test, "%H:%M:%S")
+hpc$test <- strptime(hpc$Time, format = "%H:%M:%S", tz = "GMT")
+hpc$Time <- format(hpc$test, "%H:%M:%S")
 
 hpc$Date.Time <- with(hpc, paste(Date,Time))
 hpc$Date.Time <- strptime(hpc$Date.Time, "%Y-%m-%d %H:%M:%S")
 
 png(filename = "plot4.png",
     width = 480, 
-    height = 480 
+    height = 480,
+    bg = "transparent"
 )
 
 par(mfrow = c(2,2))
