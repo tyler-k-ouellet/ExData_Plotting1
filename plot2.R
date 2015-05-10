ffe1fe3216c8eff62a192d234002d5b1e4884896
Tyler Ouellet <tyler.ouellet@eclinicalworks.com>
2015-05-10 18:59:03 -0400

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -4,14 +4,14 @@ hpc <- read.csv("~/Downloads/household_power_consumption.txt", sep=";", na.strin
 hpc$Date <- as.Date(hpc$Date, "%d/%m/%Y")
 hpc <- hpc[hpc$Date <= as.Date("2007-02-02") & hpc$Date >= as.Date("2007-02-01"), ]
 hpc$Time <- strptime(hpc$Time, format = "%H:%M:%S", tz = "GMT")
-hpc$Time <- format(test, "%H:%M:%S")
 
 hpc$Date.Time <- with(hpc, paste(Date,Time))
 hpc$Date.Time <- strptime(hpc$Date.Time, "%Y-%m-%d %H:%M:%S")
 
 png(filename = "plot2.png",
     width = 480, 
-    height = 480 
+    height = 480,
+    bg = "transparent" 
 )
 
 with(hpc, plot(Date.Time,Global_active_power, type = "l", xlab = "", ylab = "Global Active Power (kilowatts)"))
